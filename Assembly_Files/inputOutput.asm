d1dc1bb301789bba84e1e2e074bde18dc2b37683
Leo Lwin <123371143+leolwin999@users.noreply.github.com>
2025-04-17 22:18:38 +0630

    Update inputOutput.asm

--- Assembly_Files/inputOutput.asm
+++ Assembly_Files/inputOutput.asm
@@ -6,43 +6,43 @@ section .data
 	msg_len equ $ - msg
 
 section .bss
-	input resb 128				; Reserve 128 bytes for user input
+	input resb 128			; Reserve 128 bytes for user input
 
 section .text
 	global _start
 
 _start:
 	; Print the Prompt
-	mov rax, 1				; sys_write
-	mov rdi, 1				; STDOUT
-	mov rsi, prompt				; Address of the prompt
-	mov rdx, prompt_len 			; Length of the prompt
+	mov rax, 1			; sys_write
+	mov rdi, 1			; STDOUT
+	mov rsi, prompt			; Address of the prompt
+	mov rdx, prompt_len 		; Length of the prompt
 	syscall
 
 	; Read user input
-	mov rax, 0				; sys_read
-	mov rdi, 0				; STDIN
-	mov rsi, input				; Address of the buffer
-	mov rdx, 128				; Input size
+	mov rax, 0			; sys_read
+	mov rdi, 0			; STDIN
+	mov rsi, input			; Address of the buffer
+	mov rdx, 128			; Input size
 	syscall
-	mov r8, rax				; Save the number of bytes read into r8
+	mov r8, rax			; Save the number of bytes read into r8
 
 	; Print "You entered: "
-	mov rax, 1				; sys_write
-	mov rdi, 1				; STDOUT
-	mov rsi, msg				; Addess of the message
-	mov rdx, msg_len			; Length of the message
+	mov rax, 1			; sys_write
+	mov rdi, 1			; STDOUT
+	mov rsi, msg			; Addess of the message
+	mov rdx, msg_len		; Length of the message
 	syscall
 
-	mov rax, 1				; sys_write
-	mov rdi, 1				; STDOUT
-	mov rsi, input				; Address of the buffer (user input)
-	mov rdx, r8				; Use the saved value in r8
+	mov rax, 1			; sys_write
+	mov rdi, 1			; STDOUT
+	mov rsi, input			; Address of the buffer (user input)
+	mov rdx, r8			; Use the saved value in r8
 	syscall
 
 	; Exit program
-	mov rax, 60				; sys_exit
-	xor rdi, rdi				; Exit code 0
+	mov rax, 60			; sys_exit
+	xor rdi, rdi			; Exit code 0
 	syscall
 
 
