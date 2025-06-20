0855cd44c4ba4a84ef67244cc0c3aa19bef006fc
Leo Lwin <123371143+leolwin999@users.noreply.github.com>
2025-06-20 18:32:59 +0630

    Update client.asm

--- Assembly_Files/client.asm
+++ Assembly_Files/client.asm
@@ -155,5 +155,5 @@ _read_error:
 
 _exit:
         mov rax, 60                                                     ; Syscall number for exit
-        mov rdi, rdi                                                    ; Sets rdi to 0
+        xor rdi, rdi                                                    ; Sets rdi to 0
         syscall                                                         ; Call kernel
